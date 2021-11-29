ba64027e57552abe426b9b9f8686f636fb1db9f2
Eduardo Moraes <eduardomoraes2213@hotmail.com>
2021-11-29 01:43:39 -0300

    importação de imagens

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react'; // importação para usar react
-
+import Logo from './componentes/img/logo.jpg'; // o primeiro argumento é como será chamado no programa
 
 // função que retorna algo
-export default  function App(){               
+export default  function App(){  
+  
+  // Arrow function 
+  const ano =()=>{
+    return (2021)
+  }
+
+  const nome = 'Mário'
+  const idade = 20
   return(
     <>
       <div class="row">
-        <p>Teste retorno </p>
 
+        <img src={Logo}/>
+
+        <img src='/img/aeronave.jpg'/>
+
+        <p>Bem vindo {nome}, sua idade é {idade}? </p>
+
+        <p>todos direitos reservados - {ano()}</p>
       </div>
-
-      <>
-        <div>
-          <h1>Lista </h1>
-          <ul>
-            <li>Opção 1</li>
-            <li>Opção 2</li>
-          </ul>
-        </div>
-      </>
-      
     </>
   )
 }
