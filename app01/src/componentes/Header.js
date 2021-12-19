5d2fb8aa15516cfb712a760851eeca05026e0464
Eduardo Moraes <eduardomoraes2213@hotmail.com>
2021-12-19 01:15:44 -0300

    css inline, css com constantes e css externo

--- app01/src/componentes/Header.js
+++ app01/src/componentes/Header.js
@@ -3,10 +3,10 @@ import Logo from './img/logo.jpg'
 
 export default function Header(){
 
-
+    // Css inline
     return(
         <header>
-            <img src={Logo}/>
+            <img style={{width:'150px', height:'80px'}} src={Logo}/>
             
             <h1>Menu</h1>
         </header>
--- app01/src/componentes/body.js
+++ app01/src/componentes/body.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import '../App.css' // importação de arquivo css
 
 
 export default function Body(){
 
+    //css com contantes
+    const estilosLista={
+        //color:'#r3e'
+        color:'#939',
+        fontSize:'0.9rem'
+    }
+
     return(
 
         <body>
-            <h2>Curso React </h2>
-            <ul>
+            <h2 style={{fontSize:'1.5rem', color:'blueviolet'}}>Curso React </h2>
+            <ul style={estilosLista}>
                 <li>Instalação de ambiente de desenvolvimento</li>
                 <li>Conhecendo Estrutura</li>
                 <li>Funções</li>
                 <li>Componentes</li>
             </ul>
+
+            <p className='texto'>Classe css externa</p>
         </body>
+        
     )
 }
